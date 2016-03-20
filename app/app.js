f9ef292e8a3cf63b08686249bd3445553cf6538f
Run Run Shaw <webpac@ad.cityu.edu.hk>
2016-03-20 20:23:19 +0800

    Create index_html main_mod sub_mod_Ctrl skeleton

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
 'use strict';
 
-
+angular.module("myTube", [
+	'myTube.videolist'
+	]);
---
+++ app/videolist/videolist.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module("myTube.videolist",[])
+	.controller('videolistCtrl', ['$scope', function($scope){
+		
+	}])
