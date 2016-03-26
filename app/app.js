b38355bb9c2a5314d647308c41ed11e8ac6530a3
Run Run Shaw <webpac@ad.cityu.edu.hk>
2016-03-26 18:35:58 +0800

    Use ng-view

--- app/app.js
+++ app/app.js
@@ -3,4 +3,9 @@
 angular.module("myTube", [
 	'myTube.videolist',
 	'myTube.modelservices'
-	]);
+	]).
+config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when("/", {templateUrl: "videolist/videolist.html", controller: "videolistCtrl"}).
+		otherwise({redirectTo: "/"});
+}]);
--- app/videolist/videolist.js
+++ app/videolist/videolist.js
@@ -2,7 +2,7 @@
 
 angular.module("myTube.videolist",[])
 	.controller('videolistCtrl', ['$scope', 'getVideos', 'YT_VIDEO_URL', function($scope, getVideos, YT_VIDEO_URL){
-		var url = YT_VIDEO_URL;
+		var url = YT_VIDEO_URL; // todo: need to move url back in services.js later
     getVideos(url).then(function(results) {
         $scope.videos = results;
      });
@@ -10,8 +10,7 @@ angular.module("myTube.videolist",[])
 	}])
 
 // Todo:
-// Refactoring to a separate service, so the controller can use xxx.then()
-// Refactor the url to constant
+
 // Click to create movie view page
 
 // Todo2:
